c08b7fd25084e8b62591313957f838669bb08866
Stephanie Kim <stephaniekim@Stephanies-MacBook-Air.local>
2025-07-05 12:09:13 -0700

    Update header href with window origin path

--- js/header.js
+++ js/header.js
@@ -11,13 +11,13 @@ const headerHTML = `
             <h3 class="artwork-menu-text">ARTWORK ▾</h3>
             <div class="artwork-sublinks">
                 <h4><a href="${rootPath}/exhibits">EXHIBITS</a></h4>
-                <h4><a href="sculptural">SCULPTURAL</a></h4>
-                <h4><a href="functional">FUNCTIONAL</a></h4>
-                <h4><a href="petportraits">PET PORTRAITS</a></h4>
+                <h4><a href="${rootPath}/sculptural">SCULPTURAL</a></h4>
+                <h4><a href="${rootPath}/functional">FUNCTIONAL</a></h4>
+                <h4><a href="${rootPath}/petportraits">PET PORTRAITS</a></h4>
             </div>
-            <h3><a class="menu-text" href="/bio">BIO</a></h3>
-            <h3><a class="menu-text" href="/cv">CV</a></h3>
-            <h3><a class="menu-text" href="/contact">CONTACT</a></h3>
+            <h3><a class="menu-text" href="${rootPath}/bio">BIO</a></h3>
+            <h3><a class="menu-text" href="${rootPath}/cv">CV</a></h3>
+            <h3><a class="menu-text" href="${rootPath}/contact">CONTACT</a></h3>
         </div>
     </div>
     <div class="header">
@@ -25,16 +25,16 @@ const headerHTML = `
         <div class="header-bar">
             <div class="header-item" id="artwork">ARTWORK
                 <ul class="drop-down-container">
-                    <li class="drop-down-item"><a href="exhibits">EXHIBITS</a></l>
-                        <li class="drop-down-item"><a href="sculptural">SCULPTURAL</a></li>
-                        <li class="drop-down-item"><a href="functional">FUNCTIONAL</a></li>
+                    <li class="drop-down-item"><a href="${rootPath}/exhibits">EXHIBITS</a></l>
+                        <li class="drop-down-item"><a href="${rootPath}/sculptural">SCULPTURAL</a></li>
+                        <li class="drop-down-item"><a href="${rootPath}/functional">FUNCTIONAL</a></li>
                         <li id="portraits" class="drop-down-item"><a href=
-                                "petportraits">PET PORTRAITS</a></li>
+                                "${rootPath}/petportraits">PET PORTRAITS</a></li>
                 </ul>
             </div>
-            <div class="header-item"><a href="bio">BIO</a></div>
-            <div class="header-item"><a href="cv">CV</a></div>
-            <div class="header-item"><a href="contact">CONTACT</a></div>
+            <div class="header-item"><a href="${rootPath}/bio">BIO</a></div>
+            <div class="header-item"><a href="${rootPath}/cv">CV</a></div>
+            <div class="header-item"><a href="${rootPath}/contact">CONTACT</a></div>
         </div>
     </div>`;
 
